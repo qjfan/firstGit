bf525bed7f71e503bd3f75cbc4d64303e6262b93
Administrator <Administrator@USER-20170516UM>
2017-08-21 19:18:14 +0800

    同string去点

---
+++ firstGit/src/com/qisheng/Work.java
@@ -0,0 +1,24 @@
+package com.qisheng;
+
+public class Work {
+	
+	private String username;
+	
+	private int age;
+	
+	public static void main(String[] args) {
+		
+		System.out.println("work");
+	
+		
+	}
+
+
+	
+	
+	
+	
+	
+	
+
+}
