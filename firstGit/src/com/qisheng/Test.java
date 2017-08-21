26c5d3e29f37abfa670e3f409fc30bd1e64a0f31
Administrator <Administrator@USER-20170516UM>
2017-08-21 19:26:46 +0800

    添加分支的方法，bean包

--- firstGit/src/com/qisheng/Test.java
+++ firstGit/src/com/qisheng/Test.java
@@ -21,10 +21,22 @@ public class Test {
 
 
 
+	public int getI() {
+		return i;
+	}
 
 
 
+	public void setI(int i) {
+		this.i = i;
+	}
 
+
+
+	@Override
+	public String toString() {
+		return "Test [i=" + i + "]";
+	}
 
 
 
