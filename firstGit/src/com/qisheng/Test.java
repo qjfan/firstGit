08db04b169e45f868787f26dddfd3f890030f90f
Administrator <Administrator@USER-20170516UM>
2017-08-21 18:56:11 +0800

    toString()

--- firstGit/src/com/qisheng/Test.java
+++ firstGit/src/com/qisheng/Test.java
@@ -8,9 +8,22 @@ public class Test {
 	public static int add(int x,int y) {
 		
 		return x + y;
+		
 	}
 	
 	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
+
+
+
+
+
+
 	public static void main(String[] args) {
 		
 		add(4,2);
